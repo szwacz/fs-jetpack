2b874fc1073c79547e10e431e933b83fbf3b6d1c
Jakub Szwacz <jakub@szwacz.com>
2013-10-20 20:30:01 +0200

    dirAsync() first draft

--- spec/dir.spec.js
+++ spec/dir.spec.js
@@ -81,13 +81,103 @@ describe('dir', function () {
         
     });
     
-    xdescribe('async', function () {
+    describe('async', function () {
         
-        it('', function () {
+        it('should make sure dir exist', function () {
             var done = false;
-            
-            
-            
+            expect(fse.existsSync('something')).toBe(false);
+            jetpack.dirAsync('something')
+            .then(function () {
+                expect(fse.existsSync('something')).toBe(true);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('should make sure many nested dirs exist', function () {
+            var done = false;
+            expect(fse.existsSync('something')).toBe(false);
+            jetpack.dirAsync('something/other')
+            .then(function () {
+                expect(fse.existsSync('something')).toBe(true);
+                expect(fse.existsSync('something/other')).toBe(true);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('should make sure dir does not exist', function () {
+            var done = false;
+            fse.mkdirSync('something');
+            expect(fse.existsSync('something')).toBe(true);
+            jetpack.dirAsync('something', { exists: false })
+            .then(function () {
+                expect(fse.existsSync('something')).toBe(false);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('should not bother about dir emptiness if not said so', function () {
+            var done = false;
+            fse.mkdirsSync('something/other');
+            expect(fse.existsSync('something/other')).toBe(true);
+            jetpack.dirAsync('something')
+            .then(function () {
+                expect(fse.existsSync('something/other')).toBe(true);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('should make sure dir is empty', function () {
+            var done = false;
+            fse.mkdirsSync('something/other');
+            expect(fse.existsSync('something/other')).toBe(true);
+            jetpack.dirAsync('something', { empty: true })
+            .then(function () {
+                expect(fse.existsSync('something/other')).toBe(false);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('if exists = false, empty should be ignored', function () {
+            var done = false;
+            fse.mkdirsSync('something/other');
+            expect(fse.existsSync('something/other')).toBe(true);
+            jetpack.dirAsync('something', { exists: false, empty: true })
+            .then(function () {
+                expect(fse.existsSync('something')).toBe(false);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('if given path is file, should delete it and place dir instead', function () {
+            var done = false;
+            fse.createFileSync('something');
+            expect(fse.statSync('something').isFile()).toBe(true);
+            jetpack.dirAsync('something')
+            .then(function () {
+                expect(fse.statSync('something').isDirectory()).toBe(true);
+                done = true;
+            });
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it('can chain dir calls, and should return new CWD context', function () {
+            var done = false;
+            jetpack.dirAsync('something')
+            .then(function (context) {
+                expect(context.cwd()).toBe(pathUtil.resolve(process.cwd(), 'something'));
+                return context.dirAsync('else');
+            })
+            .then(function (context) {
+                expect(context.cwd()).toBe(pathUtil.resolve(process.cwd(), 'something/else'));
+                expect(fse.existsSync('something/else')).toBe(true);
+                done = true;
+            });
             waitsFor(function () { return done; }, null, 200);
         });
         
