2598e9cb3a8bdf332dfca00b8c43b9bb90c0d9e5
Jakub Szwacz <jakub@szwacz.com>
2013-12-10 21:02:03 +0100

    list amended

--- spec/list.spec.js
+++ spec/list.spec.js
@@ -20,7 +20,9 @@ describe('list', function () {
         it("should return array of objects", function () {
             fse.mkdirSync('a');
             fse.writeFileSync('b.txt', 'abc');
+            
             var list = jetpack.list('.');
+            
             expect(list.length).toBe(2);
             expect(list[0].name).toBe('a');
             expect(list[0].type).toBe('dir');
@@ -31,7 +33,9 @@ describe('list', function () {
         it("should list subdirectories if such option specified", function () {
             fse.mkdirpSync('a/b/c');
             fse.writeFileSync('a/d.txt', 'abc');
+            
             var list = jetpack.list('.', { subDirs: true });
+            
             expect(list[0].children[0].type).toBe('dir');
             expect(list[0].children[0].name).toBe('b');
             expect(list[0].children[0].path).toBe(pathUtil.join(jetpack.cwd(), 'a', 'b'));
@@ -48,11 +52,32 @@ describe('list', function () {
         
         it("should start with root object, not only its content if *includeRoot*", function () {
             fse.mkdirpSync('a/b');
+            
             var tree = jetpack.list('a', { includeRoot: true, subDirs: true });
+            
             expect(tree.type).toBe('dir');
             expect(tree.name).toBe('a');
             expect(tree.children[0].type).toBe('dir');
             expect(tree.children[0].name).toBe('b');
+        });
+        
+        it("should have size information", function () {
+            fse.mkdirpSync('a');
+            fse.mkdirpSync('b/c');
+            fse.writeFileSync('b/c/file1.txt', 'abc');
+            fse.writeFileSync('b/file2.txt', 'xy');
+            
+            var tree = jetpack.list('a', { includeRoot: true, subDirs: true });
+            
+            expect(tree.size).toBe(0); // is empty dir
+            
+            tree = jetpack.list('b', { includeRoot: true, subDirs: true });
+            
+            expect(tree.size).toBe(5); // 5 bytes combined
+            expect(tree.children[0].name).toBe('c');
+            expect(tree.children[0].size).toBe(3);
+            expect(tree.children[1].name).toBe('file2.txt');
+            expect(tree.children[1].size).toBe(2);
         });
         
     });
@@ -61,11 +86,13 @@ describe('list', function () {
         
         it("should list empty directory", function () {
             var done = false;
+            
             jetpack.listAsync('.')
             .then(function (list) {
                 expect(list.length).toBe(0);
                 done = true;
             });
+            
             waitsFor(function () { return done; }, null, 200);
         });
         
@@ -73,6 +100,7 @@ describe('list', function () {
             var done = false;
             fse.mkdirSync('a');
             fse.writeFileSync('b.txt', 'abc');
+            
             jetpack.listAsync('.')
             .then(function (list) {
                 expect(list.length).toBe(2);
@@ -82,6 +110,7 @@ describe('list', function () {
                 expect(list[1].type).toBe('file');
                 done = true;
             });
+            
             waitsFor(function () { return done; }, null, 200);
         });
         
@@ -89,6 +118,7 @@ describe('list', function () {
             var done = false;
             fse.mkdirpSync('a/b/c');
             fse.writeFileSync('a/d.txt', 'abc');
+            
             jetpack.listAsync('.', { subDirs: true })
             .then(function (list) {
                 expect(list[0].children[0].type).toBe('dir');
@@ -105,12 +135,14 @@ describe('list', function () {
                 expect(list[0].children[1].parent).toBe(list[0]);
                 done = true;
             });
+            
             waitsFor(function () { return done; }, null, 200);
         });
         
         it("should start with root object, not only its content if *includeRoot*", function () {
             var done = false;
             fse.mkdirpSync('a/b');
+            
             jetpack.listAsync('a', { includeRoot: true, subDirs: true })
             .then(function (tree) {
                 expect(tree.type).toBe('dir');
@@ -119,6 +151,32 @@ describe('list', function () {
                 expect(tree.children[0].name).toBe('b');
                 done = true;
             });
+            
+            waitsFor(function () { return done; }, null, 200);
+        });
+        
+        it("should have size information", function () {
+            var done = false;
+            fse.mkdirpSync('a');
+            fse.mkdirpSync('b/c');
+            fse.writeFileSync('b/c/file1.txt', 'abc');
+            fse.writeFileSync('b/file2.txt', 'xy');
+            
+            jetpack.listAsync('a', { includeRoot: true, subDirs: true })
+            .then(function (tree) {
+                expect(tree.size).toBe(0); // is empty dir
+                
+                return jetpack.listAsync('b', { includeRoot: true, subDirs: true });
+            })
+            .then(function (tree) {
+                expect(tree.size).toBe(5); // 5 bytes combined
+                expect(tree.children[0].name).toBe('c');
+                expect(tree.children[0].size).toBe(3);
+                expect(tree.children[1].name).toBe('file2.txt');
+                expect(tree.children[1].size).toBe(2);
+                done = true;
+            });
+            
             waitsFor(function () { return done; }, null, 200);
         });
         
