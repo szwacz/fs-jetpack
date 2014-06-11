25891ed1f0f45e257e47908db0d87f590625ff08
Jakub Szwacz <jakub@szwacz.com>
2014-06-11 21:05:04 +0200

    exists.spec rewritten

--- spec/exists.spec.js
+++ spec/exists.spec.js
@@ -10,63 +10,47 @@ describe('exists', function () {
     beforeEach(helper.beforeEach);
     afterEach(helper.afterEach);
 
-    describe('sync', function () {
-
-        it("returns false if file doesn't exist", function () {
-            expect(fse.existsSync('something.txt')).toBe(false);
-            var exists = jetpack.exists('something.txt');
+    it("returns false if file doesn't exist", function (done) {
+        // SYNC
+        var exists = jetpack.exists('file.txt');
+        expect(exists).toBe(false);
+        
+        // ASYNC
+        jetpack.existsAsync('file.txt')
+        .then(function (exists) {
             expect(exists).toBe(false);
+            done();
         });
-
-        it("returns 'dir' if directory exists on given path", function () {
-            fse.mkdirsSync('something');
-            var exists = jetpack.exists('something');
-            expect(exists).toBe('dir');
-        });
-
-        it("returns 'file' if file exists on given path", function () {
-            fse.outputFileSync('something.txt', 'abc');
-            var exists = jetpack.exists('something.txt');
-            expect(exists).toBe('file');
-        });
-
     });
 
-    describe('async', function () {
-
-        it("returns false if file doesn't exist", function () {
-            var done = false;
-            expect(fse.existsSync('something.txt')).toBe(false);
-            jetpack.existsAsync('something.txt')
-            .then(function (exists) {
-                expect(exists).toBe(false);
-                done = true;
-            });
-            waitsFor(function () { return done; }, null, 200);
+    it("returns 'dir' if directory exists on given path", function (done) {
+        fse.mkdirsSync('a');
+        
+        // SYNC
+        var exists = jetpack.exists('a');
+        expect(exists).toBe('dir');
+        
+        // ASYNC
+        jetpack.existsAsync('a')
+        .then(function (exists) {
+            expect(exists).toBe('dir');
+            done();
         });
+    });
 
-        it("returns 'dir' if directory exists on given path", function () {
-            var done = false;
-            fse.mkdirsSync('something');
-            jetpack.existsAsync('something')
-            .then(function (exists) {
-                expect(exists).toBe('dir');
-                done = true;
-            });
-            waitsFor(function () { return done; }, null, 200);
+    it("returns 'file' if file exists on given path", function (done) {
+        fse.outputFileSync('text.txt', 'abc');
+        
+        // SYNC
+        var exists = jetpack.exists('text.txt');
+        expect(exists).toBe('file');
+        
+        // ASYNC
+        jetpack.existsAsync('text.txt')
+        .then(function (exists) {
+            expect(exists).toBe('file');
+            done();
         });
-
-        it("returns 'file' if file exists on given path", function () {
-            var done = false;
-            fse.outputFileSync('something.txt', 'abc');
-            jetpack.existsAsync('something.txt')
-            .then(function (exists) {
-                expect(exists).toBe('file');
-                done = true;
-            });
-            waitsFor(function () { return done; }, null, 200);
-        });
-
     });
 
 });
