03fd2d7c46f9c4be7ab7041a5aa8b08ac455f643
Jakub Szwacz <jakub@szwacz.com>
2014-06-15 09:37:56 +0200

    read.spec rewritten

--- spec/read.spec.js
+++ spec/read.spec.js
@@ -10,83 +10,66 @@ describe('read', function () {
     beforeEach(helper.beforeEach);
     afterEach(helper.afterEach);
     
-    describe('sync', function () {
+    it('reads file content as utf-8', function (done) {
+        fse.writeFileSync('file.txt', 'ąśźń', { encoding: 'utf8' });
         
-        it('should read file content as utf-8', function () {
-            fse.writeFileSync('file.txt', 'ąśźń', { encoding: 'utf8' });
-            
-            var content = jetpack.read('file.txt'); // second parameter defaults to utf8
+        // SYNC
+        // second parameter defaults to utf8
+        var content = jetpack.read('file.txt');
+        expect(content).toBe('ąśźń');
+        
+        // second parameter explicitly specified
+        content = jetpack.read('file.txt', 'utf8');
+        expect(content).toBe('ąśźń');
+        
+        // ASYNC
+        // second parameter defaults to utf8
+        jetpack.readAsync('file.txt')
+        .then(function (content) {
             expect(content).toBe('ąśźń');
             
-            content = jetpack.read('file.txt', 'utf8');
+            // second parameter explicitly specified
+            return jetpack.readAsync('file.txt', 'utf8');
+        })
+        .then(function (content) {
             expect(content).toBe('ąśźń');
+            done();
         });
+    });
+    
+    it('reads file content as Buffer', function (done) {
+        fse.writeFileSync('file.txt', new Buffer([33, 77]));
         
-        it('should read file content as Buffer', function () {
-            fse.writeFileSync('file.txt', new Buffer([33,77]));
-            
-            var content = jetpack.read('file.txt', 'buf');
+        // SYNC
+        var content = jetpack.read('file.txt', 'buf');
+        expect(Buffer.isBuffer(content)).toBe(true);
+        expect(content[0]).toBe(33);
+        expect(content[1]).toBe(77);
+        
+        // AYNC
+        jetpack.readAsync('file.txt', 'buf')
+        .then(function (content) {
             expect(Buffer.isBuffer(content)).toBe(true);
             expect(content[0]).toBe(33);
             expect(content[1]).toBe(77);
+            done();
         });
-        
-        it('should read file content as JSON', function () {
-            fse.outputJSONSync('file.txt', { a: '123' });
-            
-            var content = jetpack.read('file.txt', 'json');
-            expect(content.a).toBe('123');
-        });
-        
     });
     
-    describe('async', function () {
+    it('reads file content as JSON', function (done) {
+        var obj = { a: '123' };
+        fse.outputJSONSync('file.json', obj);
         
-        it('should read file content as utf-8', function () {
-            var done = false;
-            fse.writeFileSync('file.txt', 'ąśźń', { encoding: 'utf8' });
-            
-            jetpack.readAsync('file.txt') // second parameter defaults to utf8
-            .then(function (content) {
-                expect(content).toBe('ąśźń');
-                return jetpack.read('file.txt', 'utf8');
-            })
-            .then(function (content) {
-                expect(content).toBe('ąśźń');
-                done = true;
-            });
-            
-            waitsFor(function () { return done; }, null, 200);
+        // SYNC
+        var content = jetpack.read('file.json', 'json');
+        expect(content).toEqual(obj);
+        
+        // ASYNC
+        jetpack.readAsync('file.json', 'json')
+        .then(function (content) {
+            expect(content).toEqual(obj);
+            done();
         });
-        
-        it('should read file content as Buffer', function () {
-            var done = false;
-            fse.writeFileSync('file.txt', new Buffer([33,77]));
-            
-            jetpack.readAsync('file.txt', 'buf')
-            .then(function (content) {
-                expect(Buffer.isBuffer(content)).toBe(true);
-                expect(content[0]).toBe(33);
-                expect(content[1]).toBe(77);
-                done = true;
-            });
-            
-            waitsFor(function () { return done; }, null, 200);
-        });
-        
-        it('should read file content as JSON', function () {
-            var done = false;
-            fse.outputJSONSync('file.txt', { a: '123' });
-            
-            jetpack.readAsync('file.txt', 'json')
-            .then(function (content) {
-                expect(content.a).toBe('123');
-                done = true;
-            });
-            
-            waitsFor(function () { return done; }, null, 200);
-        });
-        
     });
     
 });
