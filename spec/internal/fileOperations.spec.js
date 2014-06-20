d512cfc63616c12c8748e6be68a50add22d28ee3
Jakub Szwacz <jakub@szwacz.com>
2014-06-20 23:23:52 +0200

    file rename

--- spec/internal/fileOperations.spec.js
+++ spec/internal/fileOperations.spec.js
@@ -1,11 +1,11 @@
 "use strict";
 
-describe('internal/file', function () {
+describe('internal/fileOperations |', function () {
     
     var fse = require('fs-extra');
     var pathUtil = require('path');
     var helper = require('../helper');
-    var internalFile = require('../../lib/internal/file');
+    var internalFile = require('../../lib/internal/fileOperations');
     
     beforeEach(helper.beforeEach);
     afterEach(helper.afterEach);
