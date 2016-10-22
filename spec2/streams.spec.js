9655dec5be1e142dfb8ce4f3204807449e9efbe4
Jakub Szwacz <jakub@szwacz.com>
2016-10-22 21:53:57 +0200

    stream tests ported to mocha

---
+++ spec2/streams.spec.js
@@ -0,0 +1,41 @@
+var fse = require('fs-extra');
+var expect = require('chai').expect;
+var helper = require('./helper');
+var jetpack = require('..');
+
+describe('streams |', function () {
+  beforeEach(helper.setCleanTestCwd);
+  afterEach(helper.switchBackToCorrectCwd);
+
+  it('exposes vanilla stream methods', function (done) {
+    var input;
+    var output;
+
+    fse.outputFileSync('a.txt', 'abc');
+
+    input = jetpack.createReadStream('a.txt');
+    output = jetpack.createWriteStream('b.txt');
+    output.on('finish', function () {
+      expect('b.txt').to.have.content('abc');
+      done();
+    });
+    input.pipe(output);
+  });
+
+  it('stream methods respect jetpack internal CWD', function (done) {
+    var input;
+    var output;
+
+    var dir = jetpack.cwd('dir');
+
+    fse.outputFileSync('dir/a.txt', 'abc');
+
+    input = dir.createReadStream('a.txt');
+    output = dir.createWriteStream('b.txt');
+    output.on('finish', function () {
+      expect('dir/b.txt').to.have.content('abc');
+      done();
+    });
+    input.pipe(output);
+  });
+});
