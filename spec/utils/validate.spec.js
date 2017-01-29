c6ceb66e402978bc8915f175d2a2db338e0fac81
Jakub Szwacz <jakub@szwacz.com>
2017-01-29 10:08:28 +0100

    Validation API amendments

--- spec/utils/validate.spec.js
+++ spec/utils/validate.spec.js
@@ -2,83 +2,85 @@ var expect = require('chai').expect;
 var validate = require('../../lib/utils/validate');
 
 describe('util validate', function () {
-  it('validates its own input', function () {
-    expect(function () {
-      validate('foo(thing)', 'thing', 123, ['foo']);
-    }).to.throw('Unknown type "foo"');
-  });
-
-  [
-    {
-      type: 'string', article: 'a', goodValue: 'abc',
-      wrongValue: 123, wrongValueType: 'number'
-    },
-    {
-      type: 'number', article: 'a', goodValue: 123,
-      wrongValue: 'abc', wrongValueType: 'string'
-    },
-    {
-      type: 'boolean', article: 'a', goodValue: true,
-      wrongValue: 'abc', wrongValueType: 'string'
-    },
-    {
-      type: 'array', article: 'an', goodValue: [],
-      wrongValue: {}, wrongValueType: 'object'
-    },
-    {
-      type: 'object', article: 'an', goodValue: {},
-      wrongValue: [], wrongValueType: 'array'
-    },
-    {
-      type: 'buffer', article: 'a', goodValue: new Buffer(1),
-      wrongValue: 123, wrongValueType: 'number'
-    },
-    {
-      type: 'null', article: 'a', goodValue: null,
-      wrongValue: 123, wrongValueType: 'number'
-    },
-    {
-      type: 'undefined', article: 'an', goodValue: undefined,
-      wrongValue: 123, wrongValueType: 'number'
-    }
-  ]
-  .forEach(function (test) {
-    it('validates that given thing is a(n) ' + test.type, function () {
+  describe('validates arguments passed to methods', function () {
+    it('validates its own input', function () {
       expect(function () {
-        validate('foo(thing)', 'thing', test.goodValue, [test.type]);
-      }).not.to.throw();
-
-      expect(function () {
-        validate('foo(thing)', 'thing', test.wrongValue, [test.type]);
-      }).to.throw('Argument "thing" passed to foo(thing) must be ' + test.article + ' ' + test.type
-        + '. Received ' + test.wrongValueType);
+        validate.argument('foo(thing)', 'thing', 123, ['foo']);
+      }).to.throw('Unknown type "foo"');
     });
-  });
 
-  [
-    { type: 'string', value: 'abc', expect: 'number' },
-    { type: 'number', value: 123, expect: 'string' },
-    { type: 'boolean', value: true, expect: 'number' },
-    { type: 'array', value: [], expect: 'number' },
-    { type: 'object', value: {}, expect: 'number' },
-    { type: 'buffer', value: new Buffer(1), expect: 'number' },
-    { type: 'null', value: null, expect: 'number' },
-    { type: 'undefined', value: undefined, expect: 'number' }
-  ]
-  .forEach(function (test) {
-    it('can detect wrong type: ' + test.type, function () {
-      expect(function () {
-        validate('foo(thing)', 'thing', test.value, [test.expect]);
-      }).to.throw('Argument "thing" passed to foo(thing) must be a '
-        + test.expect + '. Received ' + test.type);
+    [
+      {
+        type: 'string', article: 'a', goodValue: 'abc',
+        wrongValue: 123, wrongValueType: 'number'
+      },
+      {
+        type: 'number', article: 'a', goodValue: 123,
+        wrongValue: 'abc', wrongValueType: 'string'
+      },
+      {
+        type: 'boolean', article: 'a', goodValue: true,
+        wrongValue: 'abc', wrongValueType: 'string'
+      },
+      {
+        type: 'array', article: 'an', goodValue: [],
+        wrongValue: {}, wrongValueType: 'object'
+      },
+      {
+        type: 'object', article: 'an', goodValue: {},
+        wrongValue: [], wrongValueType: 'array'
+      },
+      {
+        type: 'buffer', article: 'a', goodValue: new Buffer(1),
+        wrongValue: 123, wrongValueType: 'number'
+      },
+      {
+        type: 'null', article: 'a', goodValue: null,
+        wrongValue: 123, wrongValueType: 'number'
+      },
+      {
+        type: 'undefined', article: 'an', goodValue: undefined,
+        wrongValue: 123, wrongValueType: 'number'
+      }
+    ]
+    .forEach(function (test) {
+      it('validates that given thing is a(n) ' + test.type, function () {
+        expect(function () {
+          validate.argument('foo(thing)', 'thing', test.goodValue, [test.type]);
+        }).not.to.throw();
+
+        expect(function () {
+          validate.argument('foo(thing)', 'thing', test.wrongValue, [test.type]);
+        }).to.throw('Argument "thing" passed to foo(thing) must be ' + test.article
+          + ' ' + test.type + '. Received ' + test.wrongValueType);
+      });
     });
-  });
 
-  it('supports more than one allowed type', function () {
-    expect(function () {
-      validate('foo(thing)', 'thing', {}, ['string', 'number', 'boolean']);
-    }).to.throw('Argument "thing" passed to foo(thing) must be a string'
-      + ' or a number or a boolean. Received object');
+    [
+      { type: 'string', value: 'abc', expect: 'number' },
+      { type: 'number', value: 123, expect: 'string' },
+      { type: 'boolean', value: true, expect: 'number' },
+      { type: 'array', value: [], expect: 'number' },
+      { type: 'object', value: {}, expect: 'number' },
+      { type: 'buffer', value: new Buffer(1), expect: 'number' },
+      { type: 'null', value: null, expect: 'number' },
+      { type: 'undefined', value: undefined, expect: 'number' }
+    ]
+    .forEach(function (test) {
+      it('can detect wrong type: ' + test.type, function () {
+        expect(function () {
+          validate.argument('foo(thing)', 'thing', test.value, [test.expect]);
+        }).to.throw('Argument "thing" passed to foo(thing) must be a '
+          + test.expect + '. Received ' + test.type);
+      });
+    });
+
+    it('supports more than one allowed type', function () {
+      expect(function () {
+        validate.argument('foo(thing)', 'thing', {}, ['string', 'number', 'boolean']);
+      }).to.throw('Argument "thing" passed to foo(thing) must be a string'
+        + ' or a number or a boolean. Received object');
+    });
   });
 
   describe('validates options object', function () {
