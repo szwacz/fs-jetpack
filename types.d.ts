0dbcdb5fb2e8f7d4e235eca37ff815d9fda579c0
Jakub Szwacz <jakub@szwacz.com>
2019-10-21 08:56:03 +0200

    Format

--- types.d.ts
+++ types.d.ts
@@ -85,22 +85,20 @@ type WriteOptions = {
   jsonIndent?: number;
 };
 
-
-// API has the same set of synchronous and asynchronous methods. 
+// API has the same set of synchronous and asynchronous methods.
 // All async methods are promise based (no callbacks).
 
-// Commonly used naming convention in Node world is reversed in this library (no 'method' and 'methodSync' naming). 
-// Asynchronous methods are those with 'Async' suffix, all methods without 'Async' in the name are synchronous. 
-// Reason behind this is that it gives very nice look to blocking API. And promise-based, non-blocking code is verbose 
+// Commonly used naming convention in Node world is reversed in this library (no 'method' and 'methodSync' naming).
+// Asynchronous methods are those with 'Async' suffix, all methods without 'Async' in the name are synchronous.
+// Reason behind this is that it gives very nice look to blocking API. And promise-based, non-blocking code is verbose
 // anyway, so one more word is not much of a difference.
-
 
 export interface FSJetpack {
   /**
    * Returns Current Working Directory (CWD) for this instance of jetpack, or creates new jetpack object with given path as its internal CWD.
-   * 
+   *
    * **Note:** fs-jetpack never changes value of `process.cwd()`, the CWD we are talking about here is internal value inside every jetpack instance.
-   * 
+   *
    *  @param path (optional) path (or many path parts) to become new CWD. Could be absolute, or relative. If relative path given new CWD will be resolved basing on current CWD of this jetpack instance.
    */
   cwd: {
@@ -112,19 +110,19 @@ export interface FSJetpack {
 
   /**
    * Appends given data to the end of file. If file or any parent directory doesn't exist it will be created.
-   * 
-   * @param path  the path to file.  
-   * @param data  data to append (can be `String` or `Buffer`).  
-   * @param options 
+   *
+   * @param path  the path to file.
+   * @param data  data to append (can be `String` or `Buffer`).
+   * @param options
    */
   append(path: string, data: AppendData, options?: AppendOptions): void;
-  
+
   /**
    * Appends given data to the end of file. If file or any parent directory doesn't exist it will be created.
-   * 
-   * @param path  the path to file.  
-   * @param data  data to append (can be `String` or `Buffer`).  
-   * @param options 
+   *
+   * @param path  the path to file.
+   * @param data  data to append (can be `String` or `Buffer`).
+   * @param options
    */
   appendAsync(
     path: string,
@@ -132,25 +130,23 @@ export interface FSJetpack {
     options?: AppendOptions
   ): Promise<void>;
 
-
   /**
    * Copies given file or directory (with everything inside).
-   * 
-   * @param from path to location you want to copy.  
-   * @param to path to destination location, where the copy should be placed.  
-   * @param options 
+   *
+   * @param from path to location you want to copy.
+   * @param to path to destination location, where the copy should be placed.
+   * @param options
    */
   copy(from: string, to: string, options?: CopyOptions): void;
 
   /**
    * Copies given file or directory (with everything inside).
-   * 
-   * @param from path to location you want to copy.  
-   * @param to path to destination location, where the copy should be placed.  
-   * @param options 
+   *
+   * @param from path to location you want to copy.
+   * @param to path to destination location, where the copy should be placed.
+   * @param options
    */
   copyAsync(from: string, to: string, options?: CopyOptions): Promise<void>;
-
 
   /**
    * Just an alias to vanilla [fs.createReadStream](http://nodejs.org/api/fs.html#fs_fs_createreadstream_path_options).
@@ -161,32 +157,30 @@ export interface FSJetpack {
    */
   createWriteStream: typeof fs.createWriteStream;
 
-
   /**
-   * Ensures that directory on given path exists and meets given criteria. If any criterium is not met it will be 
+   * Ensures that directory on given path exists and meets given criteria. If any criterium is not met it will be
    * after this call. If any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
+   *
    * @param path path to directory to examine
    * @param criteria criteria to be met by the directory
    */
   dir(path: string, criteria?: DirCriteria): FSJetpack;
-  
+
   /**
-   * Ensures that directory on given path exists and meets given criteria. If any criterium is not met it will be 
+   * Ensures that directory on given path exists and meets given criteria. If any criterium is not met it will be
    * after this call. If any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
+   *
    * @param path path to directory to examine
    * @param criteria criteria to be met by the directory
    */
   dirAsync(path: string, criteria?: DirCriteria): Promise<FSJetpack>;
 
-
   /**
-   * Checks whether something exists on given `path`. This method returns values more specific than `true/false` to 
+   * Checks whether something exists on given `path`. This method returns values more specific than `true/false` to
    * protect from errors like "I was expecting directory, but it was a file".
-   * 
+   *
    * @param path path to look for
-   * 
+   *
    * Returns:
    *  - `false` if path doesn't exist.
    *  - `"dir"` if path is a directory.
@@ -195,11 +189,11 @@ export interface FSJetpack {
    */
   exists(path: string): ExistsResult;
   /**
-   * Checks whether something exists on given `path`. This method returns values more specific than `true/false` to 
+   * Checks whether something exists on given `path`. This method returns values more specific than `true/false` to
    * protect from errors like "I was expecting directory, but it was a file".
-   * 
+   *
    * @param path path to look for
-   * 
+   *
    * Returns:
    *  - `false` if path doesn't exist.
    *  - `"dir"` if path is a directory.
@@ -208,25 +202,23 @@ export interface FSJetpack {
    */
   existsAsync(path: string): Promise<ExistsResult>;
 
-
   /**
-   * Ensures that file exists and meets given criteria. If any criterium is not met it will be after this call. If 
+   * Ensures that file exists and meets given criteria. If any criterium is not met it will be after this call. If
    * any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
+   *
    * @param path the path to create
    * @param criteria criteria to be met by the file
    */
   file(path: string, criteria?: FileOptions): FSJetpack;
-  
+
   /**
-   * Ensures that file exists and meets given criteria. If any criterium is not met it will be after this call. If 
+   * Ensures that file exists and meets given criteria. If any criterium is not met it will be after this call. If
    * any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
+   *
    * @param path the path to create
    * @param criteria criteria to be met by the file
    */
   fileAsync(path: string, criteria?: FileOptions): Promise<FSJetpack>;
-
 
   /**
    * Finds in directory specified by `path` all files fulfilling `searchOptions`. Returned paths are relative to current CWD of jetpack instance.
@@ -235,40 +227,39 @@ export interface FSJetpack {
   find(options?: FindOptions): string[];
   /**
    * Finds in directory specified by `path` all files fulfilling `searchOptions`. Returned paths are relative to current CWD of jetpack instance.
-   * @param startPath Path to start search in (all subdirectories will be searched).  
+   * @param startPath Path to start search in (all subdirectories will be searched).
    * @param options search options
    */
   find(startPath: string, options?: FindOptions): string[];
-    /**
+  /**
    * Finds in directory specified by `path` all files fulfilling `searchOptions`. Returned paths are relative to current CWD of jetpack instance.
    * @param options search options
    */
   findAsync(options?: FindOptions): Promise<string[]>;
-    /**
+  /**
    * Finds in directory specified by `path` all files fulfilling `searchOptions`. Returned paths are relative to current CWD of jetpack instance.
-   * @param startPath Path to start search in (all subdirectories will be searched).  
+   * @param startPath Path to start search in (all subdirectories will be searched).
    * @param options search options
    */
   findAsync(startPath: string, options?: FindOptions): Promise<string[]>;
 
-
   /**
-   * Inspects given path (replacement for `fs.stat`). Returned object by default contains only very basic, not 
-   * platform-dependent properties (so you have something e.g. your unit tests can rely on), you can enable more 
+   * Inspects given path (replacement for `fs.stat`). Returned object by default contains only very basic, not
+   * platform-dependent properties (so you have something e.g. your unit tests can rely on), you can enable more
    * properties through options object.
-   * 
+   *
    * @param path path to inspect
-   * @param options 
+   * @param options
    */
   inspect(path: string, options?: InspectOptions): InspectResult | undefined;
 
   /**
-   * Inspects given path (replacement for `fs.stat`). Returned object by default contains only very basic, not 
-   * platform-dependent properties (so you have something e.g. your unit tests can rely on), you can enable more 
+   * Inspects given path (replacement for `fs.stat`). Returned object by default contains only very basic, not
+   * platform-dependent properties (so you have something e.g. your unit tests can rely on), you can enable more
    * properties through options object.
-   * 
+   *
    * @param path path to inspect
-   * @param options 
+   * @param options
    */
   inspectAsync(
     path: string,
@@ -277,26 +268,25 @@ export interface FSJetpack {
 
   /**
    * Calls inspect recursively on given path so it creates a tree of all directories and sub-directories inside it.
-   * 
+   *
    * @param path starting path to inspect
-   * @param options 
+   * @param options
    */
   inspectTree(
     path: string,
     options?: InspectTreeOptions
   ): InspectTreeResult | undefined;
-  
+
   /**
    * Calls inspect recursively on given path so it creates a tree of all directories and sub-directories inside it.
-   * 
+   *
    * @param path starting path to inspect
-   * @param options 
+   * @param options
    */
   inspectTreeAsync(
     path: string,
     options?: InspectTreeOptions
   ): Promise<InspectTreeResult | undefined>;
-
 
   /**
    * Lists the contents of directory. Equivalent of `fs.readdir`.
@@ -311,7 +301,7 @@ export interface FSJetpack {
 
   /**
    * Moves given path to new location.
-   * 
+   *
    * @param from path
    * @param to path
    */
@@ -319,7 +309,7 @@ export interface FSJetpack {
 
   /**
    * Moves given path to new location.
-   * 
+   *
    * @param from path
    * @param to path
    */
@@ -327,7 +317,7 @@ export interface FSJetpack {
 
   /**
    * Reads content of file.
-   * 
+   *
    * @param path path to file
    * @param returnAs a custom return types
    */
@@ -337,7 +327,7 @@ export interface FSJetpack {
   read(path: string, returnAs: "json" | "jsonWithDates"): any | undefined;
   /**
    * Reads content of file.
-   * 
+   *
    * @param path path to file
    * @param returnAs a custom return types
    */
@@ -349,33 +339,31 @@ export interface FSJetpack {
     returnAs: "json" | "jsonWithDates"
   ): Promise<any | undefined>;
 
-
   /**
    * Deletes given path, no matter what it is (file, directory or non-empty directory). If path already doesn't exist
    * terminates gracefully without throwing, so you can use it as 'ensure path doesn't exist'.
-   * 
+   *
    * @param path path to delete
    */
   remove(path?: string): void;
   /**
    * Deletes given path, no matter what it is (file, directory or non-empty directory). If path already doesn't exist
    * terminates gracefully without throwing, so you can use it as 'ensure path doesn't exist'.
-   * 
+   *
    * @param path path to delete
    */
   removeAsync(path?: string): Promise<void>;
 
-
   /**
    * Renames given file or directory.
-   * 
+   *
    * @param path path to file being renamed
    * @param newName just the name of the thing being renamed
    */
   rename(path: string, newName: string): void;
-    /**
+  /**
    * Renames given file or directory.
-   * 
+   *
    * @param path path to file being renamed
    * @param newName just the name of the thing being renamed
    */
@@ -383,36 +371,35 @@ export interface FSJetpack {
 
   /**
    * Creates symbolic link.
-   * 
-   * @param symlinkValue path where symbolic link should point.  
-   * @param path  where symbolic link should be put.  
+   *
+   * @param symlinkValue path where symbolic link should point.
+   * @param path  where symbolic link should be put.
    */
   symlink(symlinkValue: string, path: string): void;
 
   /**
    * Creates symbolic link.
-   * 
-   * @param symlinkValue path where symbolic link should point.  
-   * @param path  where symbolic link should be put.  
+   *
+   * @param symlinkValue path where symbolic link should point.
+   * @param path  where symbolic link should be put.
    */
   symlinkAsync(symlinkValue: string, path: string): Promise<void>;
 
+  /**
+   * Writes data to file. If any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
+   *
+   * @param path path to file
+   * @param data data to be written. This could be `String`, `Buffer`, `Object` or `Array` (if last two used, the data will be outputted into file as JSON).
+   * @param options
+   */
+  write(path: string, data: WritableData, options?: WriteOptions): void;
 
   /**
    * Writes data to file. If any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
+   *
    * @param path path to file
-   * @param data data to be written. This could be `String`, `Buffer`, `Object` or `Array` (if last two used, the data will be outputted into file as JSON).  
-   * @param options 
-   */
-  write(path: string, data: WritableData, options?: WriteOptions): void;
-  
-  /**
-   * Writes data to file. If any parent directory in `path` doesn't exist it will be created (like `mkdir -p`).
-   * 
-   * @param path path to file
-   * @param data data to be written. This could be `String`, `Buffer`, `Object` or `Array` (if last two used, the data will be outputted into file as JSON).  
-   * @param options 
+   * @param data data to be written. This could be `String`, `Buffer`, `Object` or `Array` (if last two used, the data will be outputted into file as JSON).
+   * @param options
    */
   writeAsync(
     path: string,
