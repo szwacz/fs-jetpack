520b31d9920ec9721e311ddf04804dd8ecd0f3a0
Jakub Szwacz <jakub@szwacz.com>
2022-09-15 13:45:48 +0200

    No need to check any longer

--- spec/log.spec.ts
+++ spec/log.spec.ts
@@ -2,15 +2,11 @@ import * as util from "util";
 import { expect } from "chai";
 import * as jetpack from "..";
 
-if (util.inspect.custom !== undefined) {
-  // Test for https://github.com/szwacz/fs-jetpack/issues/29
-  // Feature `util.inspect.custom` which made fixing this possible was
-  // introduced in node v6.6.0, hence this test is runned conditionally.
-  describe("console.log", () => {
-    it("can be printed by console.log", () => {
-      expect(() => {
-        console.log(jetpack);
-      }).not.to.throw();
-    });
+// Test for https://github.com/szwacz/fs-jetpack/issues/29
+describe("console.log", () => {
+  it("can be printed by console.log", () => {
+    expect(() => {
+      console.log(jetpack);
+    }).not.to.throw();
   });
-}
+});
